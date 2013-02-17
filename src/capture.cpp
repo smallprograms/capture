12f8aa41223e52071202cbb83bf2591ef63b8117
Juan Reyes <notosil@gmail.com>
2013-02-17 14:06:09 -0300

    capture con RGB

--- src/capture.cpp
+++ src/capture.cpp
@@ -1,23 +1,25 @@
 #include <sstream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 #include <pcl/io/openni_grabber.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/common/time.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/registration/icp.h>
 #include <pcl/filters/filter.h>
 #include <pcl/filters/passthrough.h>
 
 using namespace std;
             
-        
 
 class SimpleOpenNIProcessor
 {
     public:
         SimpleOpenNIProcessor(pcl::visualization::CloudViewer& viewer);
-        void memberFunction (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud_in);
+        void cbPointCloud(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr& cloud_in);
+        void cbImage(const boost::shared_ptr<openni_wrapper::Image>& im);
+        cv::Mat getFrame (const boost::shared_ptr<openni_wrapper::Image> &img);
     private:
         pcl::visualization::CloudViewer* viewer;
 };
@@ -26,16 +28,16 @@ SimpleOpenNIProcessor::SimpleOpenNIProcessor(pcl::visualization::CloudViewer& vi
     this->viewer = &viewer;
 }
 
-void SimpleOpenNIProcessor::memberFunction(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud_in)
+void SimpleOpenNIProcessor::cbPointCloud(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr& cloud_in)
 {
     std::stringstream ss;
     static int count=0;
-    pcl::PointCloud<pcl::PointXYZ> cloud(640,480);
+    pcl::PointCloud<pcl::PointXYZRGBA> cloud(640,480);
     std::vector<int> vec;
     //filter 
     pcl::removeNaNFromPointCloud( *cloud_in, cloud, vec );
-    pcl::PointCloud<pcl::PointXYZ>::Ptr fCloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PassThrough<pcl::PointXYZ> filter;
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr fCloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
+    pcl::PassThrough<pcl::PointXYZRGBA> filter;
     filter.setInputCloud(cloud.makeShared());
     filter.setFilterFieldName("z");
     //0 to 1 m
@@ -52,7 +54,21 @@ void SimpleOpenNIProcessor::memberFunction(const pcl::PointCloud<pcl::PointXYZ>:
     }    
 }
 
+void SimpleOpenNIProcessor::cbImage(const boost::shared_ptr<openni_wrapper::Image>& im) {
+    //cv::imwrite("adf.jpg",getFrame(im));
+}
 
+cv::Mat SimpleOpenNIProcessor::getFrame (const boost::shared_ptr<openni_wrapper::Image> &img) {
+
+  cv::Mat frameRGB=cv::Mat(img->getHeight(),img->getWidth(),CV_8UC3);
+
+  img->fillRGB(frameRGB.cols,frameRGB.rows,frameRGB.data,frameRGB.step);
+  cv::Mat frameBGR;
+  cv::cvtColor(frameRGB,frameBGR,CV_RGB2BGR);
+
+  return frameBGR;
+
+}
 
 int main(int argc, char** argv)
 {
@@ -66,9 +82,13 @@ int main(int argc, char** argv)
     SimpleOpenNIProcessor processor(viewer);
 
     //connect some method of our custom class with the OpenNI grabber
-    boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> member_function =
-        boost::bind (&SimpleOpenNIProcessor::memberFunction, &processor, _1);
-    grabber->registerCallback(member_function);
+    boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> pcbPointCloud =
+        boost::bind (&SimpleOpenNIProcessor::cbPointCloud, &processor, _1);
+
+    boost::function<void (const boost::shared_ptr<openni_wrapper::Image>&)> pcbImage =
+        boost::bind (&SimpleOpenNIProcessor::cbImage, &processor, _1);
+    grabber->registerCallback(pcbPointCloud);
+    grabber->registerCallback(pcbImage);
 
     grabber->start();
     while(true);
